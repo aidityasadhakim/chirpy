a18c247107d69b103e829cbdc847e03c681abfe0
Aidityas Adhakim <aidityasadhakim250@gmail.com>
2025-05-19 11:13:30 +0700

    add api and metrics

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
-	// This is a placeholder for the main function.
-	// The actual implementation will depend on the specific requirements of the application.
-	fmt.Print("Hello, World!")
+	mux := http.NewServeMux()
+	filepathRoot := "."
+	port := ":8080"
+	server := &http.Server{
+		Addr:    port,
+		Handler: mux,
+	}
+	apiCfg := &apiConfig{}
+
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerResetMetrics)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
 }
